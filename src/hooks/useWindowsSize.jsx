b4a769648acd7f7330dc062ceba86d86cc109f40
Gabriel Lopes Pereira <54251267+lopix320@users.noreply.github.com>
2024-05-31 16:48:07 -0300

    feat: implementa um hook que me retorna o width e o height atual do site

---
+++ src/hooks/useWindowsSize.jsx
@@ -0,0 +1,26 @@
+import { useState, useEffect } from 'react';
+
+function useWindowSize() {
+  const [windowSize, setWindowSize] = useState({
+    width: window.innerWidth,
+    height: window.innerHeight,
+  });
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowSize({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    // Cleanup listener on component unmount
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  return windowSize;
+}
+
+export default useWindowSize;
